22e577ef32cdd6489fe8cd62ec5f2682878b219f
howevert <1413537501@qq.com>
2020-07-24 16:35:27 +0800

    [parking lot][david] refactor: modify should_return_ticket_when_parking_given_a_car

--- src/test/java/com/oocl/cultivation/test/ParkingBoyFacts.java
+++ src/test/java/com/oocl/cultivation/test/ParkingBoyFacts.java
@@ -10,23 +10,25 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class ParkingBoyFacts {
     @Test
     void should_return_ticket_when_parking_given_a_car() {
-        ParkingBoy parkingBoy=new ParkingBoy();
-        Car car=new Car();
+        ParkingBoy parkingBoy = new ParkingBoy();
+        Car car = new Car();
 
-        Ticket ticket=parkingBoy.giveTicket(car);
+        Ticket ticket = parkingBoy.giveTicket(car);
 
 
-        assertEquals("1234",ticket.getId());
+        assertEquals("1234", ticket.getCarId());
     }
 
     @Test
     void should_return_car_when_fetching_car_given_a_ticket() {
-        ParkingBoy parkingBoy=new ParkingBoy();
-        String carNum="1234";
-        Ticket ticket=new Ticket(carNum);
+        ParkingBoy parkingBoy = new ParkingBoy();
+        String carNum = "1234";
+        Ticket ticket = new Ticket(carNum);
 
-        Car car=parkingBoy.giveCar(ticket);
+        Car car = parkingBoy.giveCar(ticket);
 
-        assertEquals("1234",car.getCarId());
+        assertEquals("1234", car.getCarId());
     }
+
+
 }
