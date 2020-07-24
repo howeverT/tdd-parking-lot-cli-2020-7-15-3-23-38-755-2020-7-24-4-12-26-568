87003bf75481783d83431791c4b6bdac6be93f0f
HoweverT <1413537501@qq.com>
2020-07-24 20:28:19 +0800

    [parking lot][david] feature: past should_return_cant_get_car_when_fetching_car_given_ticket_5678_to_get_car_1_but_give_no_ticket

--- src/test/java/com/oocl/cultivation/test/ParkingLogFacts.java
+++ src/test/java/com/oocl/cultivation/test/ParkingLogFacts.java
@@ -25,5 +25,17 @@ public class ParkingLogFacts {
 
     }
 
+    @Test
+    void should_return_cant_get_car_when_fetching_car_given_ticket_5678_to_get_car_1_but_give_no_ticket() {
+        ParkingLot parkingLot = new ParkingLot();
+        HashSet<Ticket> ticketHashSet = new HashSet<>();
+        ticketHashSet.add(new Ticket("1234", "C001"));
+        ticketHashSet.add(new Ticket("5678", "C002"));
+        Ticket wrongTicket = null;
 
+        parkingLot.setTickets(ticketHashSet);
+        String isRight = parkingLot.isRightTicket(wrongTicket);
+
+        assertEquals("can't get car.", isRight);
+    }
 }
