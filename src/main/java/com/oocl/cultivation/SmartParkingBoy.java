c4207f82e59e974e1743b3abac3e0eba3569f543
HoweverT <1413537501@qq.com>
2020-07-25 21:42:48 +0800

    [parking lot][david] refactor: test should_return_C001_park_in_the_park_lot_1_and_C002_park_in_the_park_lot_2_when_parking_car_given_2_car

---
+++ src/main/java/com/oocl/cultivation/SmartParkingBoy.java
@@ -0,0 +1,11 @@
+package com.oocl.cultivation;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SmartParkingBoy extends ParkingBoy {
+    @Override
+    public String selectPark(List<ParkingLot> parkingLotList, Ticket ticket) {
+        return null;
+    }
+}
---
+++ src/test/java/com/oocl/cultivation/test/SmartParkingBoyFacts.java
@@ -0,0 +1,28 @@
+package com.oocl.cultivation.test;
+
+import com.oocl.cultivation.ParkingLot;
+import com.oocl.cultivation.SmartParkingBoy;
+import com.oocl.cultivation.Ticket;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SmartParkingBoyFacts {
+    @Test
+    void should_return_C001_park_in_the_park_lot_1_and_C002_park_in_the_park_lot_2_when_parking_car_given_2_car() {
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy();
+        List<ParkingLot> parkingLots = new ArrayList<>();
+        parkingLots.add(new ParkingLot());
+        parkingLots.add(new ParkingLot());
+
+        String resultOne = smartParkingBoy.selectPark(parkingLots, new Ticket("C001"));
+        String resultTwo = smartParkingBoy.selectPark(parkingLots, new Ticket("C002"));
+
+        assertEquals("Car C001 park in parking lot 1", resultOne);
+        assertEquals("Car C002 park in parking lot 2", resultTwo);
+
+    }
+}
