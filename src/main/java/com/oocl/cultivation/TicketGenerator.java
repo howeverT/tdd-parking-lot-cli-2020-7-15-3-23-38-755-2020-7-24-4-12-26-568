79bd9305eb3704b177f22c7f75af592f8eacff67
HoweverT <1413537501@qq.com>
2020-07-26 13:47:17 +0800

    [parking lot][david] feature: finish method generate in TicketGenerator

--- src/main/java/com/oocl/cultivation/TicketGenerator.java
+++ src/main/java/com/oocl/cultivation/TicketGenerator.java
@@ -1,7 +1,27 @@
 package com.oocl.cultivation;
 
+import java.util.HashSet;
+import java.util.List;
+
 public class TicketGenerator {
-    public String generate(){
-        return String.valueOf((int)((Math.random()*9+1)*1000));
+    List<ParkingLot> parkingLots;
+    HashSet<String> ticketNumSet;
+
+    public void setParkingLots(List<ParkingLot> parkingLots) {
+        this.parkingLots = parkingLots;
+        for (ParkingLot parkingLot : this.parkingLots) {
+            for (Ticket ticket : parkingLot.getTickets()) {
+                ticketNumSet.add(ticket.getId());
+            }
+        }
+    }
+
+    public String generate() {
+        String generateTicketNum = String.valueOf((int) ((Math.random() * 9 + 1) * 1000));
+        while (ticketNumSet != null && ticketNumSet.contains(generateTicketNum)) {
+            generateTicketNum = String.valueOf((int) ((Math.random() * 9 + 1) * 1000));
+        }
+
+        return generateTicketNum;
     }
 }
--- src/test/java/com/oocl/cultivation/test/GeneratorTest.java
+++ src/test/java/com/oocl/cultivation/test/GeneratorTest.java
@@ -1,7 +1,11 @@
 package com.oocl.cultivation.test;
 
+import com.oocl.cultivation.ParkingLot;
 import com.oocl.cultivation.TicketGenerator;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -9,6 +13,9 @@ public class GeneratorTest {
     @Test
     void should_return_4_when_generate_number() {
         TicketGenerator ticketGenerator = new TicketGenerator();
+        List<ParkingLot> parkingLots=new ArrayList<>();
+        parkingLots.add(new ParkingLot());
+        ticketGenerator.setParkingLots(parkingLots);
 
         String result = ticketGenerator.generate();
 
