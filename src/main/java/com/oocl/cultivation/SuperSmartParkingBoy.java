7601d9cafb9af812a27f3636b32e80d1c0b10aa9
HoweverT <1413537501@qq.com>
2020-07-26 11:19:11 +0800

    [parking lot][david] feature: pass should_return_C001_park_in_the_park_lot_1_and_C002_park_in_the_park_lot_1_when_parking_car_given_2_car

--- src/main/java/com/oocl/cultivation/SuperSmartParkingBoy.java
+++ src/main/java/com/oocl/cultivation/SuperSmartParkingBoy.java
@@ -5,6 +5,23 @@ import java.util.List;
 public class SuperSmartParkingBoy extends ParkingBoy {
     @Override
     public String selectPark(List<ParkingLot> parkingLotList, Ticket ticket) {
-        return null;
+        if (parkingLotList == null) {
+            return "no park.";
+        }
+        float[] placeOfLot = new float[parkingLotList.size()];
+        int placeIndex = 0;
+        int morePlace = 0;
+        for (ParkingLot parkingLot : parkingLotList) {
+            placeOfLot[placeIndex++] = parkingLot.getTickets().size()/10;
+        }
+        for (int selectIndex = 0; selectIndex < placeOfLot.length - 1; selectIndex++) {
+            if (placeOfLot[selectIndex + 1] < 10 && placeOfLot[selectIndex] < 10 && placeOfLot[selectIndex] > placeOfLot[selectIndex + 1])
+                morePlace = selectIndex + 1;
+        }
+        if (morePlace != -1)
+            parkingLotList.get(morePlace).getTickets().add(ticket);
+        else
+            return checkPosition(parkingLotList);
+        return String.format("Car %s park in parking lot %d", ticket.getCarId(), morePlace + 1);
     }
 }
