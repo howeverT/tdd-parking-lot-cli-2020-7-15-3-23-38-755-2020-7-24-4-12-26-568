e7afe54b9f230d6d76c3eb47d238de9ee28304ae
HoweverT <1413537501@qq.com>
2020-07-26 09:39:24 +0800

    [parking lot][david] feature: pass should_return_C003_park_in_the_park_lot_1_when_parking_car_given_3_car

--- src/test/java/com/oocl/cultivation/test/SmartParkingBoyFacts.java
+++ src/test/java/com/oocl/cultivation/test/SmartParkingBoyFacts.java
@@ -25,4 +25,18 @@ public class SmartParkingBoyFacts {
         assertEquals("Car C002 park in parking lot 2", resultTwo);
 
     }
+
+    @Test
+    void should_return_C003_park_in_the_park_lot_1_when_parking_car_given_3_car() {
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy();
+        List<ParkingLot> parkingLots = new ArrayList<>();
+        parkingLots.add(new ParkingLot());
+        parkingLots.add(new ParkingLot());
+
+        smartParkingBoy.selectPark(parkingLots, new Ticket("C001"));
+        smartParkingBoy.selectPark(parkingLots, new Ticket("C002"));
+        String result = smartParkingBoy.selectPark(parkingLots, new Ticket("C003"));
+
+        assertEquals("Car C003 park in parking lot 1", result);
+    }
 }
